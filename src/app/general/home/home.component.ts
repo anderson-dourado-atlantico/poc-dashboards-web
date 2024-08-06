763b7efae0c05bd414e8fc31d0c048280b04f86e
Anderson Dourado <anderson_dourado@atlantico.com.br>
2024-08-06 16:42:35 -0300

    Incluindo estilos e pagina HOME

--- src/app/general/home/home.component.ts
+++ src/app/general/home/home.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatRippleModule } from '@angular/material/core'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatIconModule } from '@angular/material/icon'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'poc-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatRippleModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private router: Router) {}
+
+  goToDashboard() {
+    this.router.navigate(['dashboard'])
+  }
+}
